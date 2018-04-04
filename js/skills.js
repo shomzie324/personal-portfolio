1e7b7b21e071988a150624aceabf645dc53d6864
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-04-03 22:06:31 -0400

    Added animations for all skills icons

--- js/skills.js
+++ js/skills.js
@@ -3,54 +3,31 @@
 // in the function call event.target to get the icone that triggered
 // the function. Then use e.target.className to get that events class name
 // then but all possible classNames in a switch case to change the right class
+// get a list of the icons and add hover event listeners
+var icons = document.getElementById("icon-wrap").getElementsByTagName('i');
+Array.from(icons).forEach(function(icon) {
+    icon.addEventListener('mouseover', addColor);
+    icon.addEventListener('mouseout', removeColor);
+})
 
 
-function addColor(){
-    // Change logo colour
-    var njs = document.getElementById("njs");
-    document.getElementById("icon-name").innerHTML = "Node JS"
-    njs.className = "devicon-nodejs-plain colored";
-    njs.style.fontSize = "150px"; 
+//get the element the mouse is on and change it to the colored icon
+function addColor(e) {
+    var hoverIcon = e.target;
+    var name = hoverIcon.className;
+    var newName = name + ' colored';
+    var title = hoverIcon.nextElementSibling;
+    hoverIcon.className = newName;
+    hoverIcon.style.fontSize = "150px";
+    title.style.display = "block";
 }
 
-function removeColor(){
-    // Change logo colour
-    var njs = document.getElementById("njs");
-    njs.className = "devicon-nodejs-plain";
-    njs.style.fontSize = "100px";    
-    document.getElementById("icon-name").innerHTML = ""          
-}
-
-function addBSColor(){
-    // Change Bootstrap logo colour
-    var boots = document.getElementById("boots");
-    document.getElementById("bs-icon-name").innerHTML = "Bootstrap"
-    boots.className = "devicon-bootstrap-plain colored";
-    boots.style.fontSize = "150px"; 
-    boots.style.opacity = 1;
-}
-
-function removeBSColor(){
-     // Change logo colour
-     var boots = document.getElementById("boots");
-     boots.className = "devicon-bootstrap-plain";
-     boots.style.fontSize = "100px";    
-     document.getElementById("bs-icon-name").innerHTML = ""
-}
-
-function addhtColor(){
-    // Change Bootstrap logo colour
-    var ht5 = document.getElementById("ht5");
-    document.getElementById("ht-icon-name").innerHTML = "HTML 5"
-    ht5.className = "devicon-html5-plain colored";
-    ht5.style.fontSize = "150px"; 
-    ht5.style.opacity = 1;
-}
-
-function removehtColor(){
-     // Change logo colour
-     var ht5 = document.getElementById("ht5");
-     ht5.className = "devicon-html5-plain";
-     ht5.style.fontSize = "100px";    
-     document.getElementById("ht-icon-name").innerHTML = ""
+function removeColor(e) {
+    var hoverIcon = e.target;
+    var name = hoverIcon.className;
+    var newName = name.replace(' colored', '');
+    var title = hoverIcon.nextElementSibling;
+    hoverIcon.className = newName;
+    hoverIcon.style.fontSize = "100px";
+    title.style.display = "none";
 }
